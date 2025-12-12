41cba1cdeb8adf534f43210754170fcc6f84d331
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-12-12 16:20:48 +0100

    equipment

--- src/systems/equipment.js
+++ src/systems/equipment.js
@@ -2,10 +2,10 @@ import { EQUIPMENT_SLOTS } from "../data/equipmentSlots.js";
 
 export function equipItem(character, item) {
 
-    // 1.
 
-    if (character.level < item.levelRequired || item.allowedClasses.length > 0 && !item.allowedClasses.includes(character.classType)) return;
-
+    if (character.level < item.levelRequired || item.allowedClasses.length > 0 && !item.allowedClasses.includes(character.classType)) {
+        return false;
+    }
 
 
 
